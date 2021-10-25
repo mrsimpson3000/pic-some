dcc240548f47f3c30498d4c88c5c143fa92ceca9
Chad Simpson <mrsimpson3000@gmail.com>
2021-10-25 15:36:55 -0700

    React router installed and setup.

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function Cart() {
-  <main className="cart-page">
-    <h1>Check out</h1>
-  </main>;
+  return (
+    <main className="cart-page">
+      <h1>Check out</h1>
+    </main>
+  );
 }
