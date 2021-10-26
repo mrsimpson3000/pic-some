64eac2c7dcb9d509d19c6a0e9b7c1aa06533ba39
Chad Simpson <mrsimpson3000@gmail.com>
2021-10-26 15:37:46 -0700

    Now have the fetch Get request set up and is returning images from the API.

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Context = React.createContext();
 
 function ContextProvider({ children }) {
-  return <Context.Provider value="">{children}</Context.Provider>;
+  const [allPhotos, setAllPhotos] = useState([]);
+  const url =
+    "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setAllPhotos(data));
+  }, []);
+
+  return <Context.Provider value={{ allPhotos }}>{children}</Context.Provider>;
 }
 
 export { ContextProvider, Context };
