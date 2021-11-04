75e659b4aa2c05d6096061d3c922b9ce0cde099f
Chad Simpson <mrsimpson3000@gmail.com>
2021-11-04 12:23:23 -0700

    The favorited and cart icon will persist if clicked.

--- src/Context.js
+++ src/Context.js
@@ -31,9 +31,19 @@ function ContextProvider({ children }) {
     setCartItems((prevItems) => [...prevItems, newItem]);
   }
 
+  function removeFromCart(id) {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
   return (
     <Context.Provider
-      value={{ allPhotos, toggleFavorite, addToCart, cartItems }}
+      value={{
+        allPhotos,
+        toggleFavorite,
+        addToCart,
+        cartItems,
+        removeFromCart,
+      }}
     >
       {children}
     </Context.Provider>
--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,8 @@ import { Context } from "../Context";
 
 function Image({ className, img }) {
   const [hovered, setHovered] = useState(false);
-  const { toggleFavorite, addToCart, cartItems } = useContext(Context);
+  const { toggleFavorite, addToCart, cartItems, removeFromCart } =
+    useContext(Context);
 
   function heartIcon() {
     if (img.isFavorite) {
@@ -26,7 +27,12 @@ function Image({ className, img }) {
   function cartIcon() {
     const alreadyInCart = cartItems.some((item) => item.id === img.id);
     if (alreadyInCart) {
-      return <i className="ri-shopping-cart-fill cart"></i>;
+      return (
+        <i
+          className="ri-shopping-cart-fill cart"
+          onClick={() => removeFromCart(img.id)}
+        ></i>
+      );
     } else if (hovered) {
       return (
         <i
@@ -35,7 +41,6 @@ function Image({ className, img }) {
         ></i>
       );
     }
-    // Do something
   }
 
   return (
