b68524a134699a113076947d953bb97077f1b6b8
Chad Simpson <mrsimpson3000@gmail.com>
2021-11-04 14:30:36 -0700

    A mock order can be placed now.

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,20 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../Context";
 import CartItem from "../components/Cartitem";
 
 export default function Cart() {
-  const { cartItems } = useContext(Context);
+  const [buttonText, setButtonText] = useState("Place Order");
+  const { cartItems, emptyCart } = useContext(Context);
   const cartItemElements = cartItems.map((item) => (
     <CartItem key={item.id} item={item} />
   ));
+  const totalCost = 5.99 * cartItems.length;
+  const totalCostDisplay = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  function placeOrder() {
+    setButtonText("Ordering...");
+    setTimeout(() => {
+      alert("Order Placed!");
+      setButtonText("Place Order");
+      emptyCart();
+    }, 3000);
+  }
 
   return (
     <main className="cart-page">
       <h1>Check out</h1>
       {cartItemElements}
-      <p className="total-cost">Total: </p>
+      <p className="total-cost">Total: {totalCostDisplay}</p>
       <div className="order-button">
-        <button>Place Order</button>
+        <button onClick={placeOrder}>{buttonText}</button>
       </div>
     </main>
   );
